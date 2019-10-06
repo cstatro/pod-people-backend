1e0acb3512898eabce2b2fa5c13119ac319c5ae6
Colin <cstatro@gmail.com>
2019-10-06 16:00:39 -0400

    got the config set up

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module PodPeopleBack
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -26,6 +28,12 @@ module PodPeopleBack
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
