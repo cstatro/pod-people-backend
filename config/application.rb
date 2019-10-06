a6c14ba910388d099163056c887a3a96e118c836
Colin <cstatro@gmail.com>
2019-10-06 17:15:11 -0400

    decode encode helper methods have added to the lib and auto loaded

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module PodPeopleBack
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+
+    config.autoload_paths << Rails.root.join('lib')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -34,7 +37,7 @@ module PodPeopleBack
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
-
+    
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
---
+++ lib/json_web_token.rb
@@ -0,0 +1,12 @@
+class JsonWebToken
+    
+
+    def self.encode(payload)
+        JWT.encode(payload, ENV["SECRET"])
+    end
+
+    def self.decode(token)
+        JWT.decode(token,ENV["SECRET"])
+    end
+   
+end
